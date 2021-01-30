4ad709692048a3c05d1098c584c18f45fa0eafa2
porot07 <ciscomalware@github.com>
2021-01-31 00:47:21 +0500

    complete third homework

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,18 +1,18 @@
 import s from './index.module.css';
 import classnames from 'classnames';
 
-const Layout = ({ id = 0, title = '', descr = '', urlBg = false, colorBg = '#FFFFFF' }) => {
+const Layout = ({ id, title, urlBg = false, colorBg = '#FFFFFF', children }) => {
   const background = urlBg ? { backgroundImage: `url(${urlBg})` } : { backgroundColor: colorBg };
   return (
-    <section className={classnames(s.root)} id={id} style={background}>
-      <div className={classnames(s.wrapper)}>
+    <section className={s.root} id={id} style={background}>
+      <div className={s.wrapper}>
         <article>
           <div className={s.title}>
             <h3>{title}</h3>
-            <span className={classnames(s.separator)}></span>
+            <span className={s.separator}></span>
           </div>
           <div className={classnames(s.desc, s.full)}>
-            <p>{descr}</p>
+            {children}
           </div>
         </article>
       </div>
